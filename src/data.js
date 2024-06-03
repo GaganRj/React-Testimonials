61d7aaee145907d5c04ff37c69963b758a1d4ed4
myusername <141042388+GaganRj@users.noreply.github.com>
2024-06-03 09:59:34 +0530

    slider

---
+++ src/data.js
@@ -0,0 +1,47 @@
+import gagan from '../src/img/GaganRaj.png'
+import mu from '../src/img/COLOR_POP.jpg'
+import ch from '../src/img/WhatsApp Image 2024-05-20 at 11.52.14 AM.jpeg'
+import sa from '../src/img/WhatsApp Image 2024-05-20 at 11.48.11 AM.jpeg'
+import de from '../src/img/WhatsApp Image 2024-05-20 at 11.49.11 AM.jpeg'
+
+
+const reviews = [
+  {
+    id: 1,
+    name: "Musheer",
+    job: "Backend Developer",
+    image: mu,
+    text: "Experienced Backend Developer specializing in building robust, scalable server-side applications with expertise in databases, APIs, and server architecture.",
+  },
+  {
+    id: 2,
+    name: "Chandana",
+    job: "Frontend Developer",
+    image: ch,
+    text: "Passionate Frontend Developer with expertise in crafting intuitive, responsive web interfaces using HTML, CSS, JavaScript, and modern frameworks",
+  },
+
+  {
+    id: 3,
+    name: "Gaganraj",
+    job: "Frontend Developer",
+    image: gagan,
+    text: "Passionate Frontend Developer with expertise in crafting intuitive, responsive web interfaces using HTML, CSS, JavaScript, and modern frameworks",
+  },
+  {
+    id: 4,
+    name: "Sagar",
+    job: "Full stack Developer",
+    image: sa,
+    text: "Versatile Full Stack Developer adept at creating seamless web applications with proficiency in both frontend technologies and backend systems",
+  },
+  {
+    id: 5,
+    name: "Deepak",
+    job: "Backend Developer",
+    image: de,
+    text: "Experienced Backend Developer specializing in building robust, scalable server-side applications with expertise in databases, APIs, and server architecture.",
+  },
+];
+
+export default reviews;
